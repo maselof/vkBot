a476f51331dcd431fe143aaba024ccf915f59059
maselof <mr.gindeev@gmail.com>
2023-05-10 16:31:07 +0300

    delete_token

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 	vkBot2 "vkBot/events/vkBot"
 )
 
-// vk1.a.owa2Y13dceUGm_v7zbzkOU-HR5qwQFi1PgPWpm_nK6prpzxQwmVhFbCERY7y6JfC4y9AiRMBTVLTVcaAZONIBY8pvL9KVwNmfEhBvao3x0arwngrsSRdtPGCX5VAkk99wEri7Z67v-7_CvRuqgCUGMUzxntNXYDh0xIikrq5vltEbvnhcZTrSJEa-kbiwFA4kwSJb9xPgu5raOQ5I01x2A
-
 func main() {
 	token := mustToken()
 
